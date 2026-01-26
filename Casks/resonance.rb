52d1154ac5ed6ad2b380c825c112f0da2c2188ce
Dennis Blümer <dennis.bluemer@ducart.de>
2026-01-27 00:20:26 +0100

    Update resonance.rb

--- Casks/resonance.rb
+++ Casks/resonance.rb
@@ -1,8 +1,8 @@
 cask "resonance" do
-  version "1.9.3"
-  sha256 "f60851ab7746c4e2ab197edcc4180a5e1e8323ea300a6551ca21e0dad8522eaa"
+  version "2.0.0"
+  sha256 "5ac2f29d2a533e0fc982326a8c1d9e03662ed5dc7392db4170554a62934741a8"
 
-  url "https://github.com/db-mobile/resonance/releases/download/v#{version}/Resonance-#{version}-universal.dmg"
+  url "https://github.com/db-mobile/resonance/releases/download/v#{version}/Resonance_#{version}_universal.dmg"
   name "Resonance"
   desc "A local-first, zero-account API client with excellent user experience"
   homepage "https://db-mobile.github.io/resonance/"
