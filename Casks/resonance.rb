27aa84f1e897bf1497fe3aed115677ddc335e1a4
Dennis Blümer <dennis.bluemer@ducart.de>
2026-03-25 04:55:11 +0100

    Update resonance.rb

--- Casks/resonance.rb
+++ Casks/resonance.rb
@@ -1,6 +1,6 @@
 cask "resonance" do
-  version "2.7.0"
-  sha256 "86c7cda3cc38d1408a13af98548da8698c4d38becac3340e18c429f136846c00"
+  version "2.7.1"
+  sha256 "e34037c28b2d271d98e3534c33bc395ce444dc8cf657231614141a72c8b4810e"
 
   url "https://github.com/db-mobile/resonance/releases/download/v#{version}/Resonance_#{version}_universal.dmg"
   name "Resonance"
