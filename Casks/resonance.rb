ad5c7eceb380e96ca26076bde6e2cc33cec1098c
Dennis Blümer <dennis.bluemer@ducart.de>
2026-09-01 10:21:52 +0000

    Update upstream to v3.1.11

--- Casks/resonance.rb
+++ Casks/resonance.rb
@@ -1,6 +1,6 @@
 cask "resonance" do
-  version "3.1.10"
-  sha256 "e09e56f5a1ec2be1f20310b5e619d4d2bd6418018de9534d80f01e4852f97969"
+  version "3.1.11"
+  sha256 "a93e2e24be89e72e7f6acc4b4b8a5badf60efaf72f26ce5f974cf5eade2a5444"
 
   url "https://github.com/db-mobile/resonance/releases/download/v#{version}/Resonance_#{version}_universal.dmg"
   name "Resonance"
